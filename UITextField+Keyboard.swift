c97f40fdfdaa32cf7398ac6a557715b59db0939d
William Hu <william_hu@epam.com>
2017-09-27 15:53:39 +0800

    修改键盘高度

--- UITextField+Keyboard.swift
+++ UITextField+Keyboard.swift
@@ -9,7 +9,7 @@ NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidChan
 
 
 // MARK: - Keyboard Notification
-@objc func keyboardWillShow(_ notification: NSNotification) {
+@objc func keyboardWillShow(_ notification: Notification) {
     if let rect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
         
         //rect.height
@@ -17,10 +17,10 @@ NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidChan
     }
 }
 
-@objc func keyboardWillHide(_ notification: NSNotification) {
+@objc func keyboardWillHide(_ notification: Notification) {
     budgetDetailVC?.addViewBottomConstraint.constant = 0
     keyboarydHeight = 0
 }
 
-@objc func keyboardDidChangeFrame(_ notification: NSNotification) {
+@objc func keyboardDidChangeFrame(_ notification: Notification) {
 }
